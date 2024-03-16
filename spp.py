355789e02454bd3388373edffd99672381d39542
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-03-16 18:34:49 +0530

    Update spp.py

--- spp.py
+++ spp.py
@@ -46,3 +46,16 @@ tech_list = ['AAPL', 'GOOG', 'MSFT', 'AMZN']
 end = datetime.now()
 start = datetime(end.year - 1, end.month, end.day)
 
+for stock in tech_list:
+    globals()[stock] = yf.download(stock, start, end)
+
+
+company_list = [AAPL, GOOG, MSFT, AMZN]
+company_name = ["APPLE", "GOOGLE", "MICROSOFT", "AMAZON"]
+
+for company, com_name in zip(company_list, company_name):
+    company["company_name"] = com_name
+
+df = pd.concat(company_list, axis=0)
+df.tail(10)
+

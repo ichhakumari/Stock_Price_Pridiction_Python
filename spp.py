788c4fa008e1ddae98965b3b843c9e5fb8a92935
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-03-16 18:33:28 +0530

    Update spp.py

--- spp.py
+++ spp.py
@@ -19,3 +19,12 @@ warnings.filterwarnings('ignore')
 
 !pip install -q yfinance
 
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+plt.style.use("fivethirtyeight")
+%matplotlib inline
+

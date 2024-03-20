d2f7daf8be4f062c3e40dbc97ce292d4b81e9352
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-03-20 21:35:05 +0530

    Update spp.py

--- spp.py
+++ spp.py
@@ -131,3 +131,6 @@ fig.set_figwidth(15)
 AAPL['Daily Return'].plot(ax=axes[0,0], legend=True, linestyle='--', marker='o')
 axes[0,0].set_title('APPLE')
 
+GOOG['Daily Return'].plot(ax=axes[0,1], legend=True, linestyle='--', marker='o')
+axes[0,1].set_title('GOOGLE')
+
